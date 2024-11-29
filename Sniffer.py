c6a61a21b38976985df91d9a05f443c9034a9af8
fezwect <90438294+fezwect@users.noreply.github.com>
2024-11-29 20:27:41 +0500

    Update Sniffer.py

--- Sniffer.py
+++ Sniffer.py
@@ -1,7 +1,14 @@
-from scapy.all import sniff, TCP
+import pyshark
 
-def packet_handler(packet):
-    if packet.haslayer(TCP) and len(packet) > 1000:
-        print(packet.summary())
+def process_packet(packet):
+    print("Обработан пакет: ", packet.tcp.data)  # Отладочный вывод
+    if 'TCP' in packet and int(packet.tcp.len) > 1000:
+        with open('captured_packets.txt', 'a') as f:
+            f.write(f'Packet captured: {packet.tcp.data}\n')
 
-sniff(filter="tcp port 9999", prn=packet_handler, store=0)
+
+capture = pyshark.LiveCapture(interface='Adapter for loopback traffic capture', display_filter='tcp.port == 9999 and ip.src == 127.0.0.1 and ip.dst == 127.0.0.1')
+
+print("Starting packet capture...")
+
+capture.apply_on_packets(process_packet)
